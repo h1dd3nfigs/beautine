334f08c861d38f05e65fc2c154ee68bae90ad559
Ruth Fombrun <rfombrun@gmail.com>
2015-04-08 08:24:40 -0400

    rm files with sensitive data

---
+++ tester.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* To better understand how YouTube Data API v3 works with GET requests 
+* works with  GET requests, this file with make a simple request
+* modeled after Example 1 at 
+* https://developers.google.com/youtube/v3/getting-started
+/*
+*/ 
+API_KEY = ''
+stream_context_set_default(['http' => ['ignore_errors' => true]]);
+
+$filename = 'https://www.googleapis.com/youtube/v3/videos?id=ls1IALSUFTc&key='.API_KEY.'&part=snippet,contentDetails';
+
+# $filename = 'https://www.googleapis.com/youtube/v3/videos?id=ls1IALSUFTc&key='.API_KEY.'&part=snippet%2CcontentDetails%2Cstatistics%2Cstatus';
+echo $filename.'<br><br>';
+$json_response = file_get_contents($filename);
+
+//echo $json_response ; 
+
+$yt_sample = json_decode($json_response);
+echo 'The video title is : <br>'.$yt_sample->{'items'}[0]->
+	{'snippet'}->{'title'}.'<br><br>'.
+	'The vid description is: <br>'.$yt_sample->{'items'}[0]->
+	{'snippet'}->{'description'}.'<br><br>';
+
+var_dump($yt_sample) ;
